4e6973383267ad476a2b90a7897a4b5173b29168
medamshiva20 <medamshiva20@gmail.com>
2024-08-18 11:45:35 +0530

    adding redis component

--- redis.sh
+++ redis.sh
@@ -11,6 +11,16 @@ G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
+VALIDATE(){
+if [ $1 -ne 0 ]
+then 
+    echo -e "$2...$R FAILURE $N"
+    exit 1
+else
+    echo -e "$2...$G SUCCESS $N"
+fi
+}
+
 if [ $USERID -ne 0 ]
 then
     echo -e "$R ERRROR:: Please run this script with root user $N"
