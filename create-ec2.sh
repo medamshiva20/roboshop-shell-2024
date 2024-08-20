a6d624ddcdbc549836758b771d4cf8c84ef1131e
medamshiva20 <medamshiva20@gmail.com>
2024-08-20 20:27:39 +0530

    Displaying Ec2 names

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,6 +1,6 @@
 NAMES=("MongoDB" "Catalogue" "Redis" "User" "Cart" "MySQL" "Shipping" "RabbitMQ" "Payment" "Dispatch" "Web")
 
-for i in ${$NAMES[@]}
+for i in ${NAMES[@]}
 do
     echo "NAMES: $i"
 done 
