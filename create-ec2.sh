ac962e4e0fec7b2b30cfc2c12d1916fd65331df7
medamshiva20 <medamshiva20@gmail.com>
2024-08-20 21:07:01 +0530

    Creating Ec2 names

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-NAMES=("MongoDB" "Catalogue" "Redis" "User" "Cart" "MySQL" "Shipping" "RabbitMQ" "Payment" "Dispatch" "Web")
+NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
-IMAGE_ID=ami-0b4f379183e5706b9
-SECURITY_GROUP_ID=sg-0df20b2370a23626b
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-0189d4870cc65fd55
 
-for i in ${NAMES[@]}
-do
- if [[ $i == "MongoDB" || $i == "MySQL" ]]
- then 
-     
-    INSTANCE_TYPE="t3.medium"
- else
-    INSTANCE_TYPE="t2.micro"
- fi
+
+# if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
+for i in "${NAMES[@]}"
+do  
+    if [[ $i == "mongodb" || $i == "mysql" ]]
+    then
+        INSTANCE_TYPE="t3.medium"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
     echo "creating $i instance"
-    aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --count 1 --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
-done 
+    aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    echo "created $i instance: $IP_ADDRESS"
