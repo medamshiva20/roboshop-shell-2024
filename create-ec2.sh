84093b1d10add3b1e88f5a632a7b96bf966a879b
medamshiva20 <medamshiva20@gmail.com>
2024-08-20 20:55:01 +0530

    Creating Ec2 names

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,6 +1,17 @@
 NAMES=("MongoDB" "Catalogue" "Redis" "User" "Cart" "MySQL" "Shipping" "RabbitMQ" "Payment" "Dispatch" "Web")
+INSTANCE_TYPE= ""
+IMAGE_ID=ami-0b4f379183e5706b9
+SECURITY_GROUP_ID=sg-0df20b2370a23626b
 
 for i in ${NAMES[@]}
 do
-    echo "NAMES: $i"
+ if [ [ $i == "MongoDB" || $i == "MySQL" ] ]
+ then 
+     
+    INSTANCE_TYPE=t3.medium
+ else
+    INSTANCE_TYPE=t2.micro
+ fi
+    echo "creating $i instance"
+    aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --count 1 --security-group-ids SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done 
