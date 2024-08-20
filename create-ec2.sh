6f846b39a57f04dd545576a487626adb339e924c
medamshiva20 <medamshiva20@gmail.com>
2024-08-20 22:06:33 +0530

    Creating Ec2 names

--- create-ec2.sh
+++ create-ec2.sh
@@ -16,4 +16,5 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
     echo "creating $i instance"
+    aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE  --security-group-ids $ECURITY_GROUP_ID
 done
