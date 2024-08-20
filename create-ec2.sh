f8be2c4d63269a17a613670f77bcaccedd9a7e7e
medamshiva20 <medamshiva20@gmail.com>
2024-08-20 21:10:31 +0530

    Creating Ec2 names

--- create-ec2.sh
+++ create-ec2.sh
@@ -3,7 +3,7 @@
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-0b4f379183e5706b9
-SECURITY_GROUP_ID=sg-0189d4870cc65fd55
+SECURITY_GROUP_ID=sg-0df20b2370a23626b
 
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
